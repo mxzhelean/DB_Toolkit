fe1a77b6b1b6c1ee52a4faccd1910bac1257c7c7
EleanZh <mxzhelean@gmail.com>
2021-07-13 13:20:09 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='db_tool',
+      version='0.1',
+      description='Scripts for data ETL use',
+      url='TBD',
+      author='Elean Zhong',
+      author_email='mxzhelean@gmail.com',
+      packages=find_packages(),
+      zip_safe=False)
