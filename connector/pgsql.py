c097529a7bab281d2d6933639f884207d81a3334
EleanZh <mxzhelean@gmail.com>
2021-07-13 13:30:13 -0400

    Create pgsql.py
    data connector for PostregSQL type database

---
+++ connector/pgsql.py
@@ -0,0 +1,25 @@
+import psycopg2
+import pandas as pd
+import Config
+from ..util.data_util import cache_to_file
+
+config = Config('pgsql', ['user', 'password', 'host', 'database'])
+
+@cache_to_file
+def get_data(sql_string, *, db_name='test', filename=None, force=False):
+    config.load(db_name)
+    conn = psycopg2.connect("host={host} dbname={database} user={user} password={password}".format(**config.values))
+
+    colnames = []
+    rows = []
+    try:
+        cur = conn.cursor()
+        cur.execute(sql_string)
+        colnames = [desc[0] for desc in cur.description]
+        rows = cur.fetchall()
+    except Exception as e:
+        raise e
+    conn.close()
+    if rows:
+        return pd.DataFrame(rows, columns=colnames)
+    return None
